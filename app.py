caf544e08fc7452ec8598c59c9e66e57ffdc156c
Gabriele Barbieri <gabriele.barbieri@qucit.com>
2019-03-28 12:42:21 +0100

    isolate tacking pictures

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from flask import Flask, send_file
 from flask_restful import Resource, Api
-from picamera import PiCamera
+
 
 app = Flask(__name__)
 api = Api(app)
@@ -12,20 +12,26 @@ PLAYERS = ['BLACK', 'WHITE']
 CLOCK_TOUCHER = 0
 
 
+def take_photo(filename):
+    from picamera import PiCamera
+    with PiCamera() as camera:
+        camera.resolution = (256, 256)
+        camera.start_preview()
+        time.sleep(0.5)  # Camera warm-up time
+        camera.capture(filename)
+
+
 class Clock(Resource):
     def get(self):
         # initialize the camera and grab a reference to the raw camera capture
         filename = '/home/pi/pics/'
         filename += datetime.now().strftime("%Y%m%d_%H%M%S%f")
         filename += "_{player}.png".format(player=PLAYERS[CLOCK_TOUCHER])
-        with PiCamera() as camera:
-            camera.resolution = (256, 256)
-            camera.start_preview()
-            time.sleep(0.5)  # Camera warm-up time
-            camera.capture(filename)
+        take_photo(filename)
         global CLOCK_TOUCHER
         CLOCK_TOUCHER = 1 - CLOCK_TOUCHER
         return send_file(filename, mimetype='image/png')
+
 
 api.add_resource(Clock, '/clock')
 
