7b7d7612c01e5c058eb9167ff474d42fa331abbb
Gabriele Barbieri <gabriele.barbieri@qucit.com>
2019-04-01 14:10:12 +0200

    Fix deploy script

--- deploy.py
+++ deploy.py
@@ -2,12 +2,14 @@ from fabric import Connection
 
 
 PI_URL = 'pi@192.168.100.100'
-
+APP_FOLDER = '/home/pi/clockApi'
 
 c = Connection(PI_URL)
-c.run('curl -X POST http://0.0.0.0:5000/shutdown')
-c.run('/home/pi/clockApi')
-c.run('git pull -r')
-c.run('source env/bin/activate')
-c.run('pip install -r requirements.txt')
-c.run('python app.py')
+
+
+c.run('curl -X POST http://0.0.0.0:5000/shutdown', warn=True)
+c.run('cd {} && git pull -r'.format(APP_FOLDER))
+
+prefix = 'cd {} && source env/bin/activate'.format(APP_FOLDER)
+c.run('{} && pip install -r requirements.txt'.format(prefix))
+c.run('{} && python app.py &'.format(prefix))
